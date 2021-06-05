b76a504213a50179329bf02242acd2d31d505dcf
thangdinh1607 <76675039+thangdinh1607@users.noreply.github.com>
2021-06-05 21:41:53 +0700

    up

--- case-study/src/app/app.module.ts
+++ case-study/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent,
+    CustomerListComponent,
+    CustomerCreateComponent
   ],
   imports: [
     BrowserModule
